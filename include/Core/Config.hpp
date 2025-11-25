da48092565540fce7624036dbb2441e7213fa6d3
夕木 夜間 <finn.yaki.0rz@gmail.com>
2025-11-25 16:45:59 +0900

    update:  ゲームの名前を仮定する

--- include/Core/Config.hpp
+++ include/Core/Config.hpp
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 namespace Core {
-    inline constexpr const char* kWindowTitle = "Object_Boss";
+    inline constexpr const char* kWindowTitle = "Object_Boss「荒神」";
     inline constexpr const int kWindowWidth = 832;
     inline constexpr const int kWindowHeight = 768;
 }
