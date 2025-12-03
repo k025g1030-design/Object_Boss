9e3ef67f937ee99d41e1fcf7fba3a08574d1b39e
糸井 嵩人 <K025G1037@g.neec.ac.jp>
2025-12-03 11:58:33 +0900

    Revert "update:GameSceneとMenuSceneとSceneManagerのcppに入力を追加"
    This reverts commit a2f5f0c09929b19b60aa83de99b2448d824168a5.

--- include/Engine/Framework/Scene/IScene.hpp
+++ include/Engine/Framework/Scene/IScene.hpp
@@ -4,9 +4,11 @@ namespace Scene {
     class IScene {
     public:
         virtual ~IScene() = default;
-		virtual void OnEnter() = 0; // 初期化
-		virtual void Update() = 0;  // 更新
-		virtual void Render() = 0;  // 描画
-		virtual void OnExit() = 0;  // 終了
+        virtual void OnEnter() = 0;
+        virtual void OnExit() = 0;
+
+        virtual void Update() = 0;
+        virtual void Render() = 0;
     };
+
 }
--- include/Engine/Framework/Scene/MenuScene.hpp
+++ include/Engine/Framework/Scene/MenuScene.hpp
@@ -1,48 +1,14 @@
 ﻿#pragma once
 #include "Engine/Framework/Scene/IScene.hpp"
-#include <iostream>
 
 namespace Scene {
     class MenuScene : public IScene {
     public:
         void OnEnter() override;
+        void OnExit() override;
         void Update() override;
         void Render() override;
-        void OnExit() override;
-
-		
     private:
         // メニューシーン固有のメンバ変数をここに追加
-
-        void HandleInput() {
-            // 入力処理の実装
-		}
-        void UpdateAnimations() {
-            // アニメーション更新の実装
-            // ボタンのフェード&カーソル移動  
-        }
-        void DorwMenuBackground() {
-            // メニュー背景の描画処理
-		}
-        void DrowMenuBottons() {
-            // メニューボタンの描画処理
-        }   
-        void StopBGM() {
-            // BGM停止処理
-		}
-        void ReleaseUI() {
-           //Ui破棄
-        }
-        void GameLoop(IScene* currentSneme) {
-            // ゲームループ処理
-			currentSneme->OnEnter();
-            while (true) {
-                currentSneme->Update();
-                currentSneme->Render();
-				break; // 仮の終了条件
-			}
-			currentSneme->OnExit();
-		}
-
     };
 }
