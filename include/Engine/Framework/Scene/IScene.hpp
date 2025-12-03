a2f5f0c09929b19b60aa83de99b2448d824168a5
糸井 嵩人 <K025G1037@g.neec.ac.jp>
2025-12-03 11:51:56 +0900

    update:GameSceneとMenuSceneとSceneManagerのcppに入力を追加 、またMenuScene.hppに入力を 追加しました

--- include/Engine/Framework/Scene/IScene.hpp
+++ include/Engine/Framework/Scene/IScene.hpp
@@ -4,11 +4,9 @@ namespace Scene {
     class IScene {
     public:
         virtual ~IScene() = default;
-        virtual void OnEnter() = 0;
-        virtual void OnExit() = 0;
-
-        virtual void Update() = 0;
-        virtual void Render() = 0;
+		virtual void OnEnter() = 0; // 初期化
+		virtual void Update() = 0;  // 更新
+		virtual void Render() = 0;  // 描画
+		virtual void OnExit() = 0;  // 終了
     };
-
 }
